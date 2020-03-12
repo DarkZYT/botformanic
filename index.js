18ddd1e2e7f3a98dbc774c46d60087a494658472
DarkZYT <aouabsss@gmail.com>
2020-03-12 23:12:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 const prefix = "!";
+var trade;
 
 client.on('message', message => {
       var command = message.content.replace(prefix,"").split(" ")[0];
@@ -12,18 +13,19 @@ client.on('message', message => {
       var args_case = message.content.replace(prefix + command + " ","").split(" ");
       var args = message.content.toLowerCase().replace(prefix + command + " ","").split(" ");
       if(command === "trade"){
-        if(localchannel == null){
+        if(trade.started != true){
         var nums = ["0","1","2","3","4","5","6","7","8","9"]
         var code = "";
         code = nums[Math.floor(Math.random()*nums.length)] + nums[Math.floor(Math.random()*nums.length)] + nums[Math.floor(Math.random()*nums.length)] +nums[Math.floor(Math.random()*nums.length)];
         message.channel.send("Trade happening on code " + code + " in 10 seconds!");
+        trade.started = true;
         var localchannel = message.channel;
         setTimeout(function(){localchannel.send("In 5!");},5000);
         setTimeout(function(){localchannel.send("In 4!");},6000);
         setTimeout(function(){localchannel.send("In 3!");},7000);
         setTimeout(function(){localchannel.send("In 2!");},8000);
         setTimeout(function(){localchannel.send("In 1!");},9000);
-        setTimeout(function(){localchannel.send("Trade has started! Good luck!"); localchannel = null;},10000);
+        setTimeout(function(){localchannel.send("Trade has started! Good luck!"); trade.started = false;},10000);
         }
       }
 });
