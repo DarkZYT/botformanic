7ede5c27eb9c8a9c17dcdf72cb0fd18fb5ccbead
DarkZYT <aouabsss@gmail.com>
2020-03-12 23:01:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ client.on('message', message => {
       var args_case = message.content.replace(prefix + command + " ","").split(" ");
       var args = message.content.toLowerCase().replace(prefix + command + " ","").split(" ");
       if(command === "trade"){
-        message.channel.send("Yeehaw!"); 
+        var nums = ["0","1","2","3","4","5","6","7","8","9"]
+        var code = "";
+        code = nums[Math.floor(Math.random()*nums.length)] + nums[Math.floor(Math.random()*nums.length)] + nums[Math.floor(Math.random()*nums.length)] +nums[Math.floor(Math.random()*nums.length)];
+        message.channel.send("Trade happening on code " + code + " in 10 seconds!");
+        var localchannel = message.channel;
+        setTimeout(function(){localchannel.send("In 5!");},5000);
+        setTimeout(function(){localchannel.send("In 4!");},6000);
+        setTimeout(function(){localchannel.send("In 3!");},7000);
+        setTimeout(function(){localchannel.send("In 2!");},8000);
+        setTimeout(function(){localchannel.send("In 1!");},9000);
+        setTimeout(function(){localchannel.send("Trade has started!");},10000);
       }
 });
 
